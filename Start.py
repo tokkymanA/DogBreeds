93a14697607804f8e3f1fc5fd0063979062f0f6a
tokkyman <104567709+tokkymanA@users.noreply.github.com>
2024-02-14 22:18:10 +0700

    Start.py

--- Start.py
+++ Start.py
@@ -38,7 +38,6 @@ if submitted and uploaded:
     bytes_data = uploaded.getvalue()
     try:
         with st.spinner('Load model...'):
-            st.image(image = Image.open(io.BytesIO(bytes_data)))
             ModelLoad = Iden(bytes_data) 
             result = ModelLoad.result
             st.header(f"{result[0]}")
